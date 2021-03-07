4a5b3555ed94324d72eea3625ea4b8a80b79bfb7
Jenaro <jenaroaaugusto@outlook.com>
2021-03-07 20:51:52 -0300

    Graficos e tabela funcionando

--- src/app/graficos/graficos.component.ts
+++ src/app/graficos/graficos.component.ts
@@ -7,6 +7,10 @@ interface Cities {
   name: string;
   value: number;
 }
+interface Dados {
+  data: number[];
+  label: string;
+}
 
 @Component({
   selector: 'app-graficos',
@@ -15,39 +19,19 @@ interface Cities {
 })
 export class GraficosComponent implements OnInit {
   
-  constructor(){}
-  products: any = (data as any).default;
-  
-  // public barChartOptions = {
-  //   scaleShowVerticalLines: false,
-  //   responsive: true
-  // };
-  // refreshGraf(){
-    chartOptions = {
-      responsive: true,
-      
-    };
-    chartData = [
-      { data: [330, 600, 260, 700], label: 'Account A' },
-      { data: [120, 455, 100, 340], label: 'Account B' },
-      { data: [45, 67, 800, 500], label: 'Account C' }
-    ];
-    chartLabels = ['January', 'February', 'Mars', 'April'];
-  // }
- 
-  newDataPoint(dataArr = [100, 100, 100], label: string) {
-
-    this.chartData.forEach((dataset, index) => {
-      this.chartData[index] = Object.assign({}, this.chartData[index], {
-        data: [...this.chartData[index].data, dataArr[index]]
-      });
-    });
-  
-    this.chartLabels = [...this.chartLabels, label];
-  
+  constructor(){
   }
+  products: any = (data as any).default;
 
+  chartOptions = {
+    responsive: true,
+    
+  };
+  nome_cidades = this.products.map((item2: { name: any; })=> {
+    return[item2.name]  
+  });
 
+  
   model = 1;
 
 
@@ -67,9 +51,19 @@ export class GraficosComponent implements OnInit {
   collectionSize = this.products.length;
   countries!: Cities[];
 
-  
-    // this.refreshCountries();
-  // }
+  formatatdados(){
+
+    const dados =[]
+    for (let index = 0; index < this.products.length; index++) {
+      const element = this.products[index];
+      dados.push(element.value )
+      };
+    return [{label:'',data:dados}]
+
+  }
+
+  chartData =this.formatatdados();
+
 
   refreshCountries() {
     this.countries = this.products
@@ -77,6 +71,10 @@ export class GraficosComponent implements OnInit {
       .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
   ngOnInit(){
-    // console.log(data);
+ 
   }
 }
+function item(item: any, arg1: { return: { cidade: any[]; }; }) {
+  throw new Error('Function not implemented.');
+}
+
