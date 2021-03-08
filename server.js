3eb5311281be97bd2de3f469549ce1cba2d33e64
Jenaro <jenaroaaugusto@outlook.com>
2021-03-08 12:09:14 -0300

    Heruku deploy??

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const compression = require('compression')
 const path = require('path');
 
 const app = express();
+app.use(compression())
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/my-app'));
